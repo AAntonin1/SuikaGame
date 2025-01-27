e2b228a9c2da7e46bb5eb1709491d604707888ba
Antonin Angonese <antonin.ango@gmail.com>
2025-01-27 16:45:57 +0100

    Ajout du score + debut game over

--- script.js
+++ script.js
@@ -61,14 +61,29 @@ const render = Render.create({
 });
 
 const ground = Bodies.rectangle(310, 700, 620, 10, { isStatic: true });
-const leftSide = Bodies.rectangle(1, 350, 10, 700, { isStatic: true });
-const rightSide = Bodies.rectangle(620, 350, 10, 700, { isStatic: true });
+const leftSide = Bodies.rectangle(1, 490, 10, 700, { isStatic: true });
+const rightSide = Bodies.rectangle(620, 490, 10, 700, { isStatic: true });
+const hideTop = Bodies.rectangle(310, 70, 620, 140, {
+    isStatic: true,
+    isSensor: true,
+    render: {
+        fillStyle: "white",
+    }});
+const topLine = Bodies.rectangle(310, 140, 620, 5, {
+    label: "topLine",
+    isStatic: true,
+    isSensor: true,
+    render : {
+        fillStyle: "#FFEBCD",
+    }});
 
 
 let listElements = []
 listElements.push(ground);
 listElements.push(leftSide);
 listElements.push(rightSide);
+listElements.push(topLine);
+listElements.push(hideTop);
 
 
 Composite.add(engine.world, listElements);
@@ -84,43 +99,56 @@ let cursorX = 0;
 let flagOut = false;
 let TabFruits = FRUITS;
 let randomFruitsID = TabFruits[Math.floor(Math.random() * 5)];
-console.log();
+let disableCollision = false;
 addCurrentFruit(randomFruitsID);
 
 window.addEventListener("click", (e) => {
     if(flagOut) return
+    disableAction = true;
     flagOut = true;
     window.removeEventListener("mousemove", handleMouseMove);
     setTimeout(() => {
         window.addEventListener("mousemove", handleMouseMove);
-
     }, 500)
     currentFruit.isSleeping = false;
 
     setTimeout(() => {
         randomFruitsID = Math.floor(Math.random() * 5);
         addCurrentFruit(TabFruits[randomFruitsID]);
+        disableAction = false;
         flagOut = false;
     }, 500)
 })
 
 window.addEventListener("mousemove", handleMouseMove);
-
+let scoreTotal = 0;
 Event.on(engine, "collisionStart", (e) => {
     e.pairs.forEach((collision) => {
         if (collision.bodyA.label === collision.bodyB.label){
-            Composite.remove(engine.world, [collision.bodyA, collision.bodyB]);
             const xBodyA = collision.bodyA.position.x;
             const yBodyA = collision.bodyA.position.y;
             const xBodyB = collision.bodyB.position.x;
             const yBodyB = collision.bodyB.position.y;
-
             const xBodyC = (xBodyB + xBodyA) / 2;
             const yBodyC = (yBodyB + yBodyA) / 2;
             const indexCurrentF = TabFruits.findIndex(Fruit => Fruit.label === collision.bodyA.label);
+
+            const scoreDiv = document.getElementById("scoreTotal");
+
+            Composite.remove(engine.world, [collision.bodyA, collision.bodyB]);
+            scoreTotal += TabFruits[indexCurrentF].point;
+            scoreDiv.innerHTML = scoreTotal;
+
             addFruitCollid(TabFruits[indexCurrentF + 1], xBodyC, yBodyC);
 
         }
+
+        if ((collision.bodyA.label === "topLine" || collision.bodyB.label === "topLine") &&
+            !disableAction)
+        {
+            alert("Game over");
+        }
+
     })
 })
 
