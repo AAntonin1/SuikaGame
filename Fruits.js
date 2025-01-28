6de0155b2545503d438bc36769a967348eed9418
Antonin Angonese <antonin.ango@gmail.com>
2025-01-28 15:52:38 +0100

    Modif labels

---
+++ Fruits.js
@@ -0,0 +1,68 @@
+window.FRUITS = [
+    {
+        label: "planet1",
+        radius: 40 / 2,
+        color: "#F20306",
+        point: 2,
+    },
+    {
+        label: "planet2",
+        radius: 50 / 2,
+        color: "#FF624C",
+        point: 4,
+    },
+    {
+        label: "planet3",
+        radius: 72 / 2,
+        color: "#A969FF",
+        point: 6,
+    },
+    {
+        label: "planet4",
+        radius: 85 / 2,
+        color: "#FFAF02",
+        point: 8,
+    },
+    {
+        label: "planet5",
+        radius: 106 / 2,
+        color: "#FC8611",
+        point: 10,
+    },
+    {
+        label: "planet6",
+        radius: 140 / 2,
+        color: "#F41615",
+        point: 12,
+    },
+    {
+        label: "planet7",
+        radius: 160 / 2,
+        color: "#FDF176",
+        point: 14,
+    },
+    {
+        label: "planet8",
+        radius: 196 / 2,
+        color: "#FEB6AC",
+        point: 16,
+    },
+    {
+        label: "planet9",
+        radius: 220 / 2,
+        color: "#F7E608",
+        point: 18,
+    },
+    {
+        label: "planet10",
+        radius: 270 / 2,
+        color: "#89CE13",
+        point: 20,
+    },
+    {
+        label: "planet11",
+        radius: 300 / 2,
+        color: "#26AA1E",
+        point: 22,
+    },
+];
